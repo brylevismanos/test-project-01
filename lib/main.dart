812dd427ce86a202e67369b5f000201750bf2003
Bryle <brylevismanos610@gmail.com>
2022-02-07 19:33:18 +0800

    Changed initial route

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: OnboardingPage(),
-      initialRoute: 'OnboardingPage',
+      home: OnboardingPage(),
+      // initialRoute: 'OnboardingPage',
     );
   }
 }
